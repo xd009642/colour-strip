f506b89d6c82afb705270b5a1f9691f7f4583f95
xd009642 <danielmckenna93@gmail.com>
2025-04-10 13:01:46 +0900

    Make it work with stdin as well and tidy up a bit

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,51 @@
 use argh::FromArgs;
-use std::borrow::Cow;
 use regex::Regex;
-use std::sync::LazyLock;
 use std::fs::File;
 use std::io::prelude::*;
-use std::io::BufReader;
+use std::io::{self, BufReader, BufWriter};
 use std::path::PathBuf;
+use std::sync::LazyLock;
 
+/// Strips ANSI colour codes from piped input or files printing out or saving to file.
 #[derive(FromArgs)]
-/// Whatever
+#[argh(help_triggers("-h", "--help", "help"))]
 struct Args {
     /// input file
-    #[argh(option)]
+    #[argh(option, short = 'i')]
     input: Option<PathBuf>,
     /// output file
-    #[argh(option)]
+    #[argh(option, short = 'o')]
     output: Option<PathBuf>,
 }
 
-
-fn main() -> std::io::Result<()>{
+fn main() -> std::io::Result<()> {
     let args: Args = argh::from_env();
-    
-    if let Some(input) = args.input {
+
+    let reader = if let Some(input) = args.input {
         let f = File::open(input)?;
-        let mut reader = BufReader::new(f);
+        BufReader::new(Box::new(f) as Box<dyn Read>)
+    } else {
+        BufReader::new(Box::new(io::stdin()) as Box<dyn Read>)
+    };
 
-        let mut output_file = args.output.map(|x| {
-            let mut file = File::create(x).unwrap();
-            file
-        });
-
-        for line in reader.lines().filter_map(Result::ok).map(replace) {
-            if let Some(f) = &mut output_file {
-                f.write_all(line.as_bytes())?;
-                f.write_all(b"\n")?;
-            } else {
-                println!("{}", line);
-            }
+    let mut out_writer = match args.output {
+        Some(x) => {
+            let file = File::create(x).unwrap();
+            BufWriter::new(Box::new(file) as Box<dyn Write>)
         }
+        None => BufWriter::new(Box::new(io::stdout()) as Box<dyn Write>),
+    };
+
+    for line in reader.lines().filter_map(Result::ok).map(replace) {
+        out_writer.write_all(line.as_bytes())?;
+        out_writer.write_all(b"\n")?;
     }
 
     Ok(())
 }
 
-static COLOUR_CODES: LazyLock<Regex> = LazyLock::new(|| {
-    Regex::new(r#"\x1B\[[0-9;]*m"#).unwrap()
-});
+pub static COLOUR_CODES: LazyLock<Regex> =
+    LazyLock::new(|| Regex::new(r#"\x1B\[[0-9;]*m"#).unwrap());
 
 fn replace<'a>(line: String) -> String {
     (&*COLOUR_CODES).replace_all(&line, "").to_string()
